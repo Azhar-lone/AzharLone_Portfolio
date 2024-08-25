a8c638466a6ed60e9a24485d5105d59f39e21cbc
Azhar-lone <azharlonem@gmail.com>
2024-08-25 15:33:53 +0430

    0.6.8 commit

--- src/components/myUi/Nav.tsx
+++ src/components/myUi/Nav.tsx
@@ -1,21 +1,24 @@
 "use client";
 
 // importing dependencies
-import React, { useState } from "react";
-import Link from "next/link";
+import React from "react";
 import { usePathname } from "next/navigation";
 // Icons
-
+import {
+  HammerIcon,
+  BookTextIcon,
+  PhoneIcon,
+  Grid2x2Check,
+} from "lucide-react";
 // importing Shadcn Components
-// import {
-//   NavigationMenu,
-//   NavigationMenuContent,
-//   NavigationMenuIndicator,
-//   NavigationMenuItem,
-//   NavigationMenuLink,
-//   NavigationMenuTrigger,
-//   NavigationMenuList,
-// } from "@/components/ui/navigation-menu";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuTrigger,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
 //My components
 import { ModeToggle } from "./dark-mode";
 let NavItems = [
@@ -25,19 +28,23 @@ let NavItems = [
     listItems: [
       {
         text: "Projects",
-        href: "#projects",
+        href: "/#projects",
+        Icon: Grid2x2Check,
       },
       {
         text: "Skills",
-        href: "#skills",
+        href: "/#skills",
+        Icon: HammerIcon,
       },
       {
         text: "Reviews",
-        href: "#reviews",
+        href: "/#reviews",
+        Icon: BookTextIcon,
       },
       {
         text: "Contacts",
-        href: "#contacts",
+        href: "/#contacts",
+        Icon: PhoneIcon,
       },
     ],
   },
@@ -52,31 +59,50 @@ let NavItems = [
 ];
 
 const Nav: React.FC = () => {
-  const [hash, setHash] = useState<string>(window.location.hash);
   let path = usePathname();
   return (
     <div
-      className={` mx-auto sticky  justify-center top-0 shadow border-b h-[10vh] backdrop-blur flex items-center gap-4 w-full z-50`}
+      className={` mx-auto sticky   top-0 shadow border-b h-[10vh] backdrop-blur  w-full z-50 flex items-center  justify-center`}
     >
-      <ModeToggle />
-      {NavItems.map((item, index) => (
-        <Link
-          onClick={() => setHash(item.href)}
-          href={item.href}
-          key={index}
-          className={
-            hash === item.href ||
-            path === item.href ||
-            (path.includes(item.href) && item.href !== "/")
-              ? "text-foreground "
-              : "text-foreground/60 hover:text-foreground "
-          }
-        >
-          {" "}
-          {item.text}
-        </Link>
-      ))}
+      <NavigationMenu>
+        <ModeToggle />
 
+        <NavigationMenuList className="flex md:gap-5 gap-2">
+          {NavItems.map((item, index) => (
+            <NavigationMenuItem>
+              <NavigationMenuLink
+                href={item.href}
+                key={index}
+                className={
+                  path === item.href
+                    ? "opacity-100 "
+                    : "opacity-70 hover:opacity-100"
+                }
+              >
+                {" "}
+                {item.listItems && (
+                  <>
+                    <NavigationMenuTrigger>{item.text}</NavigationMenuTrigger>
+                    <NavigationMenuContent className="w-96  bg-background flex sm:gap-5 p-5 gap-1">
+                      {item.listItems.map(({ text, href, Icon }, index) => (
+                        <NavigationMenuLink
+                          key={index}
+                          href={href}
+                          className="p-2 border flex items-center flex-col "
+                        >
+                          {text}
+                          <Icon />
+                        </NavigationMenuLink>
+                      ))}
+                    </NavigationMenuContent>
+                  </>
+                )}
+                {!item.listItems && item.text}
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
     </div>
   );
 };
